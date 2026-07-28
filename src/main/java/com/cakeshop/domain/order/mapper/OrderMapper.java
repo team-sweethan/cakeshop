8e786acc2566b47c4fd544a37269ab7d03a903d6
juhwanz_w <ghdwnghks209@naver.com>
2026-07-28 14:49:05 +0900

    Test: orderMapper 테스트코드 작성

--- src/main/java/com/cakeshop/domain/order/mapper/OrderMapper.java
+++ src/main/java/com/cakeshop/domain/order/mapper/OrderMapper.java
@@ -14,12 +14,16 @@ import java.util.Optional;
 public interface OrderMapper {
     // TODO: 조회·저장 메서드 — LIMIT #{size} OFFSET #{offset} 페이징 규칙 준수
 
+    // 주문 생성
     int insertOrder(Order order);
 
+    // orde_items에 주문에 포함된 상품 1개 저장.
     int insertOrderItem(OrderItem orderItem);
 
+    //주문 상품에 선택된 옵션 하나 order_item_options에 저장.
     int insertOrderItemOption(OrderItemOption option);
 
+    // [주문 상품] 주문 제작의 참고 이미지 1개 order_item_image에 저장.
     int insertOrderItemImage(OrderItemImage image);
 
     Optional<Order> findOrderById(@Param("orderId") long orderId);
