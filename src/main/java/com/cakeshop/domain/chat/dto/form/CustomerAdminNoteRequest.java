3a02f2cd6374b66cd12817637566ba653d02dd0d
minjung0413 <nninzng@gmail.com>
2026-08-07 17:51:17 +0900

    메모 요청 notnull 어노테이션 추가

--- src/main/java/com/cakeshop/domain/chat/dto/form/CustomerAdminNoteRequest.java
+++ src/main/java/com/cakeshop/domain/chat/dto/form/CustomerAdminNoteRequest.java
@@ -13,7 +13,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerAdminNoteRequest {
-
-    // 관리자가 작성하거나 수정한 고객 특이사항 메모 내용
-    private String content;
+    @NotNull(message = "메모 요청 바디는 필수입니다.") // @NotNull 추가! (빈 문자열 ""은 허용함)
+    private String content; // 관리자가 작성하거나 수정한 고객 특이사항 메모 내용
 }
