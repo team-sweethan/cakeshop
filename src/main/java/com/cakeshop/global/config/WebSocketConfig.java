6a27fe3d86138a0c7bcd2d599759775578c7f699
sm <ssume98@naver.com>
2026-08-21 11:39:55 +0900

    fix: 운영 WebSocket origin 허용

--- src/main/java/com/cakeshop/global/config/WebSocketConfig.java
+++ src/main/java/com/cakeshop/global/config/WebSocketConfig.java
@@ -36,6 +36,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns(
+                        "http://localhost:*",
+                        "https://shinhancake.duckdns.org"
+                )
                 .withSockJS();
     }
 
